b2d495cf9acba456819f0f2eb28ef1cb7252fdcd
Darrell Best <dsbest@g.clemson.edu>
2017-02-26 20:32:20 -0500

    Updated parsing

--- asg04.py
+++ asg04.py
@@ -5,6 +5,7 @@
 #Imports#
 from mpi4py import MPI
 import numpy as np
+import csv
 
 #Globals#
 comm = MPI.COMM_WORLD
@@ -31,35 +32,84 @@ def main():
     x_all = None
 
   #Searching csv files
-  jobs_list = []
+  jobs_list = dict()
 
-  for i in range(500):
-    search(x, x_all, mpidt, jobs_list)
-
-  print("Completed Search on rank: %d" % rank)
+  #for i in range(500):
+  search(x, x_all, mpidt, jobs_list)
   #Change back to rank 0
   #if rank == 0:
     #print("All jobs: ")
-    #print(jobs_list)
+    #print(jobs_list)i
+
+#  print("Completed Search on rank: %d" % rank)
+
+#  comm.Barrier()
+#  if rank == 0:
+#    for item in jobs_list.items():
+#      print(item)
+#    print("size: %s excess: %d" % (size,(NUMBER_FILES % size)))
+
 
 #Methods#
 def search(x, x_all, mpidt, jobs_list):
   excess = NUMBER_FILES % size
   for i in range(NUMBER_FILES + excess):
-    np.put(x,[0],[(0,0,0)])
+    np.put(x,[0],[(-1,-1,-1)])
     if (i % size) == rank:
       if i < NUMBER_FILES:
         np.put(x,[0], [(i,size,rank)])
-      #print("Setting Barrier for rank: %d" % rank) 
-      #if rank == 0:
-        #print("Gathering")
+        #Build CSV file name
+        directory_name = "/scratch3/bbest/gtrace/gtrace/job_events/"
+        file_name = name_builder(i, directory_name)
+        csv_file = open(file_name)
+        csv_reader = csv.reader(csv_file)
+        #Parse file
+        if i == 1:
+         parse_csv(csv_reader)
+      
+      comm.Barrier()
       comm.Gatherv([x,mpidt], [x_all,mpidt], root=0)
       if rank == 0:
-        print("Run number: %d" % ((i/size)+1))
-        print(x_all)
-        for job in np.nditer(x_all):
-          jobs_list.append(job)
-          print(job)
+        #print("Run number: %d" % ((i/size)+1))
+        #print(x_all)
+        for job in x_all:
+          if job[0] != -1:
+            jobs_list.update({job[0]:[job[1],job[2]]})
+            #print("job elements: %s %s %s " % (job[0],job[1],job[2]))
+            
+def name_builder(i, directory_name):
+  file_name = directory_name + "part-00"
+  if i < 10:
+    file_name  = file_name + "00" + str(i)
+  elif i < 100:
+    file_name  = file_name + "0" + str(i)
+  elif i < 1000:
+    file_name  = file_name + "" + str(i)
+  file_name = file_name + "-of-00500.csv"
+  return file_name
+
+def parse_csv(csv_reader):
+  jobs_start = dict()
+  jobs_end = dict()
+  for row in csv_reader:
+    #If a job event is 0, which means it is starting
+    if (str(row[3]) == str(0)):
+      #print(row)
+      jobs_start[row[2]] = [row[3], int(row[0])]
+  #print(jobs)
+    elif (str(row[3]) != 1):
+      jobs_end[row[2]] = [row[3], int(row[0])]
+
+  for key, value in jobs_end.items():
+    print( "Key: %s  Value: %s"  % (key,value))
+    if key in jobs_start.keys():
+      jobs_start[key] = [value[0], value[1]-jobs_start[key][1]]
+      print("%s - %s = %s" % (value[1], jobs_start[key][1], value[1]-jobs_start[key][1]))
+
+    #print ("job id: %s event type: %s time stamp: %s"  % (row[2], row[3], row[0]))
+  
+
+
 
 ##########################
 if __name__ == "__main__":
